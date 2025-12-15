00297c79b2a71e34e8c5c0114dcefdc9d1663a8c
yerassyl-amangeldi <xorcer@bk.ru>
2025-12-15 22:53:50 +0500

    Initial commit: empty Spring Boot project

---
+++ src/main/java/com/example/yerassyl/YerassylApplication.java
@@ -0,0 +1,13 @@
+package com.example.yerassyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YerassylApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YerassylApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yerassyl/YerassylApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yerassyl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YerassylApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
