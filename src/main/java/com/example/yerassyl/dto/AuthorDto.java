eea3b0f8153fc4be575637939bf1b7522ce0e763
yerassyl-amangeldi <xorcer@bk.ru>
2025-12-18 04:23:03 +0500

    Add Dto

--- src/main/java/com/example/yerassyl/dto/AuthorDto.java
+++ src/main/java/com/example/yerassyl/dto/AuthorDto.java
@@ -1,4 +1,20 @@
 package com.example.yerassyl.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthorDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String nationality;
+    private String biography;
 }
