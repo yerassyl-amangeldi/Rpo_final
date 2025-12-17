df775e44b0f1d48c42dc5b6fd4df984a2ee9c96d
yerassyl-amangeldi <xorcer@bk.ru>
2025-12-18 03:13:24 +0500

    Add entity, and repository

--- src/main/java/com/example/yerassyl/repository/BookRepository.java
+++ src/main/java/com/example/yerassyl/repository/BookRepository.java
@@ -1,4 +1,20 @@
 package com.example.yerassyl.repository;
 
-public class BookRepository {
+import com.example.yerassyl.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByIsbn(String isbn);
+
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
+    @Query("SELECT b FROM Book b JOIN FETCH b.authors WHERE b.id = :id")
+    Optional<Book> findByIdWithAuthors(Long id);
 }
