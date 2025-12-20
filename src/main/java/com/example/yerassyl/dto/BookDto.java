7e9b679835dc19d8e59404391508051a8ce7d962
yerassyl-amangeldi <xorcer@bk.ru>
2025-12-20 21:00:22 +0500

    Just adding @Builder for Test improvement

--- src/main/java/com/example/yerassyl/dto/BookDto.java
+++ src/main/java/com/example/yerassyl/dto/BookDto.java
@@ -1,6 +1,7 @@
 package com.example.yerassyl.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class BookDto {
 
     private Long id;
